ad8137e4729f2068b2c6e41a1d869038ec672e5b
Abdillah Mufki Auzan Mubin <93027141+abdillahmufki@users.noreply.github.com>
2023-05-05 16:06:20 +0700

    update component

--- project/project-6/src/components/MainHeader/Navigation.js
+++ project/project-6/src/components/MainHeader/Navigation.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useContext } from "react";
 
-import classes from './Navigation.module.css';
+import classes from "./Navigation.module.css";
+import AuthContext from "../store/auth-context";
 
-const Navigation = (props) => {
+const Navigation = () => {
+  const ctxData = useContext(AuthContext);
+
   return (
     <nav className={classes.nav}>
       <ul>
-        {props.isLoggedIn && (
+        {ctxData.isLoggedIn && (
           <li>
             <a href="/">Users</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctxData.isLoggedIn && (
           <li>
             <a href="/">Admin</a>
           </li>
         )}
-        {props.isLoggedIn && (
+        {ctxData.isLoggedIn && (
           <li>
-            <button onClick={props.onLogout}>Logout</button>
+            <button onClick={ctxData.onLogout}>Logout</button>
           </li>
         )}
       </ul>
